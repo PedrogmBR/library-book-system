286b7346d4079de9d8d95c03a9ecdb367e27546e
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-09-26 22:13:26 +0800

    UPDATE: Added comments for each function for better organization and debugging, Created new methods (functions) such as displayAvailableBooks() and borrowBook() for better organization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+/*
+    Improvements Added:
+    1. Added comments for each function for better organization and debugging
+    2. Created new methods (functions) such as displayAvailableBooks() and borrowBook() for better organization
+
+    Further Suggestions:
+    1. Consider using O.O.P. (Object-Oriented Programming) classes for better code organization (best fitting for a library management system)
+*/
+
 #include <iostream>
 using namespace std;
 
+// [ METHOD ]: Display the main menu
 void showMenu() {
     cout << "=== Library Book System ===" << endl;
     cout << "1. View available books" << endl;
@@ -9,25 +19,40 @@ void showMenu() {
     cout << "Choose an option: ";
 }
 
+
+// [ METHOD ]: Display the list of available books in the library
+void displayAvailableBooks() {
+    cout << "Available books:" << endl;
+    // SUGGESTION: Create an array (a list of values) to store the list of available books instead of hard coding it
+    cout << "- The Great Gatsby" << endl;
+    cout << "- 1984" << endl;
+    cout << "- To Kill a Mockingbird" << endl;
+}
+
+// [ METHOD ]: Borrow a book from the library
+void borrowBook() {
+    cout << "You borrowed a book. Enjoy your reading!" << endl;
+    // Consider adding further implementations for borrowing a book...
+}
+
 int main() {
     int option;
     bool running = true;
 
+    // This loop runs until the user chooses to exit the program
     while (running) {
         showMenu();
         cin >> option;
 
+        // UPDATED: Moved case implementations inside new functions
         switch (option) {
             case 1:
-                cout << "Available books:" << endl;
-                cout << "- The Great Gatsby" << endl;
-                cout << "- 1984" << endl;
-                cout << "- To Kill a Mockingbird" << endl;
+                displayAvailableBooks();
                 break;
             case 2:
-                cout << "You borrowed a book. Enjoy your reading!" << endl;
+                borrowBook();
                 break;
-            case 3:
+            case 3: // Exit program
                 cout << "Exiting program. Goodbye!" << endl;
                 running = false;
                 break;
